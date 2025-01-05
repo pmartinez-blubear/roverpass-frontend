3c1a516609b4ec948e4e32ed201da5ca60222543
Pablo Martinez <pmartinez1929@gmail.com>
2025-01-05 16:52:58 -0500

    Logout session function

--- src/hooks/auth/useProvideAuth.tsx
+++ src/hooks/auth/useProvideAuth.tsx
@@ -4,13 +4,11 @@ import { axiosRequestInterceptor } from "../../interceptor/axios";
 import { handleError } from "../../utils/errorMessages";
 import { useLocation, useNavigate } from "react-router";
 import { LoginForm } from "../../interfaces/login/LoginInterface";
-
-const axiosPetition = axiosRequestInterceptor();
-
 interface LoginResponse {
   token: string;
 }
 
+const axiosPetition = axiosRequestInterceptor();
 const useProvideAuth = () => {
     const [user, setUser] = useState(getLocalStorage('user') || null);
     const [ showModalLogout, setShowModalLogout ] = useState(false);
@@ -22,29 +20,23 @@ const useProvideAuth = () => {
   
     const login = async (formLogin:LoginForm) => {
       setIsLoadingAuth(true);
-      setErrorAuth('')
-      const myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-      
-      try{
-        const data = await axiosPetition.post<LoginResponse>('/session', formLogin).catch((error) => {
-            setErrorAuth(handleError(error));
-        });
-        if(data){
-          const user = {
-            email:formLogin.email_address
-          }
-          setUser(user)
-          localStorage.setItem('user', JSON.stringify(user));
-          localStorage.setItem('token', data.data.token);
-          setShowModalLogin(false)
-          if(location.pathname === "/signin") navigate('/')
-        }
-      }catch(e){
-
-      }finally{
+      setErrorAuth('')    
+      const data = await axiosPetition.post<LoginResponse>('/session', formLogin)
+      .catch((error) => {
+          setErrorAuth(handleError(error));
+      }).finally(()=>{
         setIsLoadingAuth(false);
-      }
+      });
+      if(data){
+        const user = {
+          email:formLogin.email_address
+        }
+        setUser(user)
+        localStorage.setItem('user', JSON.stringify(user));
+        localStorage.setItem('token', data.data.token);
+        setShowModalLogin(false)
+        if(location.pathname === "/signin") navigate('/')
+      } 
     };
 
     const handleShowModalLogin = (active:boolean) => {
@@ -56,10 +48,20 @@ const useProvideAuth = () => {
     }
 
     const logout = async () => {
-     localStorage.removeItem('user');
-     localStorage.removeItem('token');
-     setUser(null);
-     setShowModalLogout(false);
+      setIsLoadingAuth(true);
+
+      const data = await axiosPetition.delete('/session')
+      .catch((error) => {
+          setErrorAuth(handleError(error));
+      }).finally(()=>{
+        setIsLoadingAuth(false);
+      });
+      if(data){
+        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        setUser(null);
+        setShowModalLogout(false);
+      }     
     };
   
     return {
